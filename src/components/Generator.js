3bdc66c8c9b4c77ab98a64ba7671f87c1dd3b364
Christopher Grainger <chris@amplified.ai>
2019-02-13 15:45:56 +1100

    Add generator base

---
+++ src/components/Generator.js
@@ -0,0 +1,40 @@
+import { faBreadLoaf } from "@fortawesome/pro-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import styled from "styled-components";
+
+const Generator = () => {
+  return (
+    <Container>
+      <Content>
+        <Toast icon={faBreadLoaf} />
+        <Text onClick={() => console.log("clicked")}>Cook some toast.</Text>
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: #405362;
+  color: #fdf4d8;
+  display: flex;
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+const Toast = styled(FontAwesomeIcon)`
+  font-size: 48pt;
+`;
+
+const Text = styled.p`
+  font-size: 36pt;
+  border-bottom: 10px solid;
+  cursor: pointer;
+`;
+
+export default Generator;
