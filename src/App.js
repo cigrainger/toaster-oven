4ad4be86bd34a859718089cfb4fb6ba24afbb710
Christopher Grainger <chris@amplified.ai>
2019-02-13 23:12:08 +1100

    Make some toast

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+import Generator from "./components/Generator";
+import Toast from "./components/Toast";
+import ToastProvider from "./components/ToastProvider";
 
-const App = () => <p>Well this is simple.</p>;
+const App = () => (
+  <ToastProvider maxToasts={3}>
+    <GlobalStyle />
+    <Generator />
+    <Toast message="test" />
+  </ToastProvider>
+);
+
+const GlobalStyle = createGlobalStyle`  
+  @import url('https://fonts.googleapis.com/css?family=Lato');
+  
+  body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    height: 100vh;
+    font-family: 'Lato', sans-serif;
+  }
+`;
 
 export default App;
