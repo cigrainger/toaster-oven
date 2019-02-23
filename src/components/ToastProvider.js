c094d5d02c315de49c9a6320a4fee05b752a851f
Christopher Grainger <chris@amplified.ai>
2019-02-24 10:49:34 +1100

    Partial out the reducer

--- src/components/ToastProvider.js
+++ src/components/ToastProvider.js
@@ -4,11 +4,17 @@ import Toast from "./Toast";
 
 export const ToastContext = React.createContext(null);
 
-const ToastProvider = ({ children }) => {
-  const [toasts, dispatch] = useReducer((state, action) => {
+const initialState = [];
+
+function partialReducer(maxToasts) {
+  return function(state, action) {
     switch (action.type) {
+      case "reset":
+        return initialState;
       case "add":
-        return [...state, { message: action.message, id: action.id }];
+        return state.length === maxToasts
+          ? [...state.slice(1), { content: action.content, id: action.id }]
+          : [...state, { content: action.content, id: action.id }];
       case "remove":
         return state.filter(({ id }) => {
           return id !== action.id;
@@ -16,9 +22,12 @@ const ToastProvider = ({ children }) => {
       default:
         return state;
     }
-  }, []);
+  };
+}
 
-  console.log(toasts);
+const ToastProvider = ({ children, maxToasts }) => {
+  const reducer = partialReducer(maxToasts);
+  const [toasts, dispatch] = useReducer(reducer, initialState);
 
   return (
     <ToastContext.Provider value={{ toasts, dispatch }}>
